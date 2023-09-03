a0486c5934e75bb79b36b77521d8e1a6af18fc89
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-09-03 19:53:12 +0400

    add index.js

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
+module.exports = router;
