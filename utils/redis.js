1c2a9d8414c105f24ccc80676dedf43726502961
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-09-03 20:35:13 +0400

    fix async

--- utils/redis.js
+++ utils/redis.js
@@ -1,38 +1,31 @@
-import { createClient } from 'redis';
-import { promisify } from 'util';
+const redis = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.client.on('error', (err) => {
-      console.error(err);
-    });
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (err) => console.log(err));
   }
 
   isAlive() {
-    return new Promise((resolve) => {
-      this.client.ping((err) => {
-        if (err) {
-          resolve(false);
-        } else {
-          resolve(true);
-        }
-      });
-    });
+    return this.client.connected;
   }
 
   async get(key) {
-    return promisify(this.client.GET).bind(this.client)(key);
+    const value = await this.getAsync(key);
+    return value;
   }
 
   async set(key, value, duration) {
-    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+    this.client.setex(key, duration, value);
   }
 
   async del(key) {
-    await promisify(this.client.DEL).bind(this.client)(key);
+    this.client.del(key);
   }
 }
 
-export const redisClient = new RedisClient();
-export default redisClient;
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
