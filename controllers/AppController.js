36456d8a68eb387bf149f6a09b7d1d6b7fe8bef9
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-09-04 11:15:18 +0400

    put controller in class

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,18 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-const getStatus = async (req, res) => {
-  const redis = await redisClient.isAlive();
-  const db = await dbClient.isAlive();
-  res.status(200).send({ redis, db });
-};
+class AppController {
+  static getStatus(req, res) {
+    const redis = redisClient.isAlive();
+    const db = dbClient.isAlive();
+    res.status(200).send({ redis, db });
+  }
 
-const getStats = async (req, res) => {
-  const users = await dbClient.nbUsers();
-  const files = await dbClient.nbFiles();
-  res.status(200).send({ users, files });
-};
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).send({ users, files });
+  }
+}
 
-module.exports = {
-  getStatus,
-  getStats,
-};
+module.exports = AppController;
